c954255c1427a747abc36eb35debac89edce1d72
Stanislaw Abyszkin <>
2019-03-31 21:16:31 +0100

    created dockerfiles for production

--- server/index.js
+++ server/index.js
@@ -7,10 +7,10 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
-app.unsubscribe(bodyParser.json())
+app.use(bodyParser.json());
 
 // Postgres Client Setup
-const {Pool} = require('pg');
+const { Pool } = require('pg');
 const pgClient = new Pool({
     user: keys.pgUser,
     host: keys.pgHost,
@@ -22,7 +22,7 @@ pgClient.on('error', () => console.log('Lost PG connection'));
 
 pgClient
     .query('CREATE TABLE IF NOT EXISTS values (number INT)')
-    .catch((err) => console.log(err))
+    .catch(err => console.log(err));
 
 // Redis Client Setup
 const redis = require('redis');
@@ -31,7 +31,6 @@ const redisClient = redis.createClient({
     port: keys.redisPort,
     retry_strategy: () => 1000
 });
-
 const redisPublisher = redisClient.duplicate();
 
 // Express route handlers
@@ -41,9 +40,9 @@ app.get('/', (req, res) => {
 });
 
 app.get('/values/all', async (req, res) => {
-    const values = await pgClient.query('SELECT * FROM values')
+    const values = await pgClient.query('SELECT * from values');
 
-    res.send(values.rows); 
+    res.send(values.rows);
 });
 
 app.get('/values/current', async (req, res) => {
@@ -55,18 +54,17 @@ app.get('/values/current', async (req, res) => {
 app.post('/values', async (req, res) => {
     const index = req.body.index;
 
-    if (parseInt(index) > 20) {
+    if (parseInt(index) > 40) {
         return res.status(422).send('Index too high');
     }
 
     redisClient.hset('values', index, 'Nothing yet!');
     redisPublisher.publish('insert', index);
-
     pgClient.query('INSERT INTO values(number) VALUES($1)', [index]);
 
-    res.send({working: true});
+    res.send({ working: true });
 });
 
 app.listen(5000, err => {
     console.log('Listening');
-})
+});
